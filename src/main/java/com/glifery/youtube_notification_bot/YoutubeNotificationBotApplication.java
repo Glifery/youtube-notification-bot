e1a54d36ae1fcb5e87c2f39b0b42579ef88c2ebc
Maksim Lysenka <glifery@gmail.com>
2022-12-08 23:36:04 +0800

    Initial commit

---
+++ src/main/java/com/glifery/youtube_notification_bot/YoutubeNotificationBotApplication.java
@@ -0,0 +1,13 @@
+package com.glifery.youtube_notification_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeNotificationBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeNotificationBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glifery/youtube_notification_bot/YoutubeNotificationBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glifery.youtube_notification_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeNotificationBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
